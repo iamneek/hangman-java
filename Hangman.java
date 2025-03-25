e972eef2e95127baba7fd5b993ce71302dccc93d
coolbit <neekkafle29@gmail.com>
2025-03-25 08:16:04 +0545

    update ascii art for hangman character

--- Hangman.java
+++ Hangman.java
@@ -4,49 +4,73 @@ import java.util.ArrayList;
 
 public class Hangman {
     static String[] hangmanFigures = {
-        """
+            """
 
 
-        """,
-        """
-             O
-
-        """,
-        """
-             O
-            -
-
-        """,
-        """
-             O
-            --
-
-        """,
-        """
-             O
-            ---
-
-        """,
-        """
-             O
-            ---
-             |
-
-        """,
-        """
-             O
-            ---
-             |
-            /
-
-        """,
-        """
-             O
-            ---
-             |
-            /\\
-
-        """
+                    """,
+            """
+                    +---+
+                    |   |
+                        |
+                        |
+                        |
+                        |
+                    =========
+                    """,
+            """
+                    +---+
+                    |   |
+                    O   |
+                        |
+                        |
+                        |
+                    =========
+                    """,
+            """
+                        +---+
+                        |   |
+                        O   |
+                        |   |
+                            |
+                            |
+                    =========
+                        """,
+            """
+                        +---+
+                        |   |
+                        O   |
+                       /|   |
+                            |
+                            |
+                    =========
+                        """,
+            """
+                      +---+
+                      |   |
+                      O   |
+                     /|\\ |
+                          |
+                          |
+                    =========
+                            """,
+            """
+                      +---+
+                      |   |
+                      O   |
+                     /|\\ |
+                     /    |
+                          |
+                    =========
+                                        """,
+            """
+                      +---+
+                      |   |
+                      O   |
+                     /|\\ |
+                     / \\ |
+                          |
+                    =========
+                                        """
     };
 
     static boolean gameOver = false;
@@ -54,6 +78,7 @@ public class Hangman {
     static String word = getWord();
     static ArrayList<Integer> matchIndexList = new ArrayList<>();
     static String[] wordFill = new String[word.length()];
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -61,7 +86,7 @@ public class Hangman {
 
         int wordLength = word.length();
 
-        System.out.println("Welcome to Hangman, your first word is "+ wordLength + " character long.\n");
+        System.out.println("Welcome to Hangman, your first word is " + wordLength + " character long.\n");
 
         for (int i = 0; i < word.length(); i++) {
             wordFill[i] = "_";
@@ -73,7 +98,7 @@ public class Hangman {
             System.out.print("\nEnter a character ( ♥ x" + life + " ): ");
             String userInputRaw = scanner.nextLine();
 
-            if(userInputRaw.length() > 1) {
+            if (userInputRaw.length() > 1) {
                 System.out.println("\nInvalid input! Please enter a single character.\n");
                 continue;
             }
@@ -82,13 +107,13 @@ public class Hangman {
             CheckWord(userInput, word);
             String userWord = String.join("", wordFill);
             if (word.equals(userWord)) {
-                System.out.println("\nYou Guessed it on the "+(7-life+1)+"th try!");
+                System.out.println("\nYou Guessed it on the " + (7 - life + 1) + "th try!");
                 gameOver = true;
             }
         }
 
         if (life == 0) {
-            System.out.println("\nYou did not guess the correct word :(\nThe word was "+ word);
+            System.out.println("\nYou did not guess the correct word :(\nThe word was " + word);
         }
 
         scanner.close();
@@ -129,29 +154,29 @@ public class Hangman {
         }
 
         for (int i = 0; i < word.length(); i++) {
-                System.out.print(wordFill[i] + " ");
+            System.out.print(wordFill[i] + " ");
         }
     }
 
-
     public static String getWord() {
-        String[] words = {"Formula", "Java", "World", "Alphabet", "Instructor", "Foreigner", "Worker",
-                        "Sophisticated", "Honor", "Information", "Technology", "Python", "Adequate", "Appropriate",
-                        "Human", "Sapiens", "Bachelor", "Contradiction", "Symphony", "Crazy", "Mother", "Brother", "Son", "Mamina"};
+        String[] words = { "Formula", "Java", "World", "Alphabet", "Instructor", "Foreigner", "Worker",
+                "Sophisticated", "Honor", "Information", "Technology", "Python", "Adequate", "Appropriate",
+                "Human", "Sapiens", "Bachelor", "Contradiction", "Symphony", "Crazy", "Mother", "Brother", "Son",
+                "Mamina" };
         String wordGen = words[ThreadLocalRandom.current().nextInt(0, words.length)];
         return wordGen;
     }
-
 
     public static void CheckWord(char userInput, String word) {
         String[] wordSplit = word.split("");
         boolean atLeastOne = false;
 
         for (int i = 0; i < wordSplit.length; i++) {
-            if (wordSplit[i].charAt(0) == userInput || wordSplit[i].charAt(0) == String.valueOf(userInput).toUpperCase().charAt(0)) {
+            if (wordSplit[i].charAt(0) == userInput
+                    || wordSplit[i].charAt(0) == String.valueOf(userInput).toUpperCase().charAt(0)) {
                 atLeastOne = true;
-                System.out.println("Character " + wordSplit[i] +" matches.");
-                if(!matchIndexList.contains(i)) {
+                System.out.println("Character " + wordSplit[i] + " matches.");
+                if (!matchIndexList.contains(i)) {
                     matchIndexList.add(i);
                 }
             }
@@ -159,7 +184,7 @@ public class Hangman {
 
         printHangman(matchIndexList);
 
-        if(!atLeastOne) {
+        if (!atLeastOne) {
             life--;
             System.out.println("");
             printHangman(matchIndexList);
